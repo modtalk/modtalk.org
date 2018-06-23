e1ca40b3abb5475a72d0dd828f790aa8dcc35e0f
Adam Jacques <adam@technowizardry.net>
2018-06-22 19:07:51 -0700

    Capture and handle OpenSSL errors

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -4,8 +4,9 @@ class DefaultController < ApplicationController
   def index
     resp = Net::HTTP.get(URI.parse("#{blog_path}/feed/"))
     @rss = SimpleRSS.parse(resp).items
-  rescue SocketError, Errno::ENETUNREACH
+  rescue SocketError, Errno::ENETUNREACH, OpenSSL::SSL::SSLError => e
     @rss = nil
+    Raven.capture_exception(e)
   end
 
   def license
